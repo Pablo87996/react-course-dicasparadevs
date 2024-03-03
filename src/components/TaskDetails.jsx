7608df91bdca30f6833ef1da908d52f3f5970778
Pablo <pablosantoscerqueira@gmail.com>
2024-03-03 19:06:19 -0300

    First commit

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+
+import Button from "./Button";
+import {useParams, useNavigate} from "react-router-dom";
+
+import "./TaskDetails.css";
+
+const TaskDetails = () => {
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const handleBackButtonClick = () => {
+        navigate(-1);
+    }
+
+    return (
+        <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+                <h2>{params.taskTitle}</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, nemo?
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default TaskDetails;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Task from "./Task";
+
+const Tasks = ({tasks, handleTaskClick, handleTaskDeletion}) => {
+    return ( // Prop drilling
+        <>
+            {tasks.map(task => <Task key={task.id} task={task} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>)}
+        </>
+    );
+}
+
+export default Tasks;
